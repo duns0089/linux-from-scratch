cd107c36541b28a826685ca1d0324918b67590dd
John Dunstan <johnlengdunstan@gmail.com>
2023-01-16 21:22:57 +1030

    packages from packages.csvs are downloaded, up to part 4 of yt (lfs chap 5)

--- LFS/lfs.sh
+++ LFS/lfs.sh
@@ -3,8 +3,8 @@ export LFS_TGT=x86_64-lfs-linux-gnu
 export LFS_DISK=/dev/sdb
 
 if ! grep -q "$LFS" /proc/mounts; then
-    source ./setupdisk.sh "$LFS_DISK"
-    sudo mkdir $LFS # make mount destination
+    # source ./setupdisk.sh "$LFS_DISK"
+    # sudo mkdir $LFS # make mount destination
     sudo mount "${LFS_DISK}2" "$LFS" # mount usb part 2 to $LFS
     sudo chown -v $USER "$LFS" # set owner before making folders
 fi
@@ -20,10 +20,15 @@ mkdir -pv $LFS/lib
 mkdir -pv $LFS/sbin
 mkdir -pv $LFS/usr
 mkdir -pv $LFS/var
-
 case $(uname -m) in
     x86_64) mkdir -pv $LFS/lib64 ;;
 esac
 
 # usb stick is now formated, mount and directories created
 
+cp -rf *.sh packages.csv "$LFS/sources"
+cd "$LFS/sources"
+echo "Now we are at: $(pwd)"
+export PATH="$LFS/tools/bin:$PATH"
+
+source download.sh
